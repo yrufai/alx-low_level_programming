ca7e1ff64a80c300d88fd7ecc13d9d648d17645d
yrufai <yrufai23@gmail.com>
2022-10-14 20:13:03 -0700

    betty ready

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,11 +16,11 @@ int main(void)
 	printf("%d is positive\n", n);
 	else if (n == 0)
 	{
-	printf("%d, is zero\n", n);
+	printf("%d is zero\n", n);
 	}
 	else
 	{
-	printf("%d, is negetive\n", n);
+	printf("%d is negetive\n", n);
 	}
 	return (0);
 }
