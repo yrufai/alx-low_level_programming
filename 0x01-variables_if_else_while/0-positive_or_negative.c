a1180c224608d5013d9c6dafc3a544a9f1b5942f
yrufai <yrufai23@gmail.com>
2022-10-15 02:40:06 +0100

    styled the betty way

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,15 +10,14 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;	
 	if (n > 0)
-	{
 	printf("%d is positive\n", n)
-	}
 	else if (n == 0)
 	{
 	printf("%d, is zero\n", n)
 	}
-	else {
-	printf(%d, is negetive\n)
+	else
+	{
+	printf("%d, is negetive\n", n)
 	}
 	return (0);
 }
