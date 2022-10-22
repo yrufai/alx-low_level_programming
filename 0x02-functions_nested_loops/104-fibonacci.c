00d7fd65c8448e253362af1b41c79534014fb96d
yrufai <yrufai23@gmail.com>
2022-10-22 09:04:08 -0700

    good for betty

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 /**
- *main return 0(successful)
- *
+ *main - prints out first 98
+ *fibonacci suit numbers
+ *Return: return 0
  */
 int main(void)
 {
-	int num1 = 0, num2 = 1;
-	int nxtTerm = num1 + num2;
-
-	for(int i = 3; i <= 98; i++)
-	{
-		printf ("%d\n", nxtTerm)
-		num1 = num2;
-		num2 = nxtTerm;
-		nxtTerm = num1 + num2;
-	}
-	return (0);
+int num1 = 0, num2 = 1;
+int nxtTerm = num1 + num2;
+for (int i = 3; i <= 98; i++)
+{
+printf("%d\n", nxtTerm);
+num1 = num2;
+num2 = nxtTerm;
+nxtTerm = num1 + num2;
+}
+return (0);
 }
