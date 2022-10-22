01b0e78017d49f50b40a009ca818a2d8aa2db4bb
yrufai <yrufai23@gmail.com>
2022-10-22 19:01:13 +0100

    betty ready

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-<<<<<<< HEAD
+
 	int num1 = 0, num2 = 1;
 	int nxtTerm = num1 + num2;
 	int i;
@@ -18,16 +18,4 @@ int main(void)
 		nxtTerm = num1 + num2;
 	}
 	return (0);
-=======
-int num1 = 0, num2 = 1;
-int nxtTerm = num1 + num2;
-for (int i = 3; i <= 98; i++)
-{
-printf("%d\n", nxtTerm);
-num1 = num2;
-num2 = nxtTerm;
-nxtTerm = num1 + num2;
-}
-return (0);
->>>>>>> 17459a1855d5f13b7eecd6ec693e563e4cd2c041
 }
