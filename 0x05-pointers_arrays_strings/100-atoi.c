b8c2ab9464d57409693476d990aa487d52397383
yrufai <yrufai23@gmail.com>
2022-10-24 20:09:40 -0700

    5-reverse string

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -18,8 +18,8 @@ num = num * 10 + (*s - '0');
 else if (num > 0)
 break;
 }
-while (*s++)
+while (*s++);
 {
-return (num *sign);
+return (num * sign);
 }
 }
--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -22,7 +22,6 @@ tmp = *(s + i);
 /**
 * _strlen - returns the length of a string
 * @s: string
-
 *
 * Return: the length of the given string
 */
@@ -31,7 +30,8 @@ int _strlen(char *s)
 {
 int len = 0;
 while (*(s + len) != '\0')
-len++;len++;len++;len++;len++;[B[B[A[A[C[C
+len++;
+}
 
 
 
