5261860991f37a5eb7e3ed85d09a441dbbba3de5
yrufai <yrufai23@gmail.com>
2022-10-24 18:45:41 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/** _strlen - returns the length of a string
+/**
+ * _strlen - returns the length of a string
 * @s: string*
 * Description: prints a _strlen
 * Return: returns length as integer;
