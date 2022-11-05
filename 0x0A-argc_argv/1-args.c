1699ab916d5a741bed9bc340a57a47b69c735715
yrufai <yrufai23@gmail.com>
2022-11-05 02:35:33 +0100

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - a program what prints the # of args passed to it
+  * @argc: the # of args
+  * @argv: the names of args
+  * Return: 0
+  *
+  *
+  */
+int main(int argc, __attribute__((unused))char **argv)
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
+
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+/**
+  * main - a program what adds positive #s
+  * @argc: the number of args
+  * @argv: the contents of args
+  *
+  * Return: 0 or 1
+  *
+  */
+int main(int argc, char **argv)
+{
+	int sum = 0;
+	int i = 1;
+	int additive = 0;
+	char *nope;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (; i < argc; i++)
+	{
+		additive = strtol(argv[i], &nope, 10);
+		if (*nope == '\0')
+		{
+			sum += additive;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+
